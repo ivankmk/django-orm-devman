d6f2bfb41f648c6236f9176daca74b51982a4d30
IvanKMK <kumeyko@hotmail.com>
2020-03-22 19:09:23 +0100

    16/16 - Done

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -3,16 +3,19 @@ from datacenter.models import Visit
 from django.shortcuts import render
 from django.utils import timezone
 
+
 def get_duration(visit):
     leaved_at = visit.leaved_at if visit.leaved_at else timezone.now()
     time_diff = leaved_at - visit.entered_at
     return time_diff
 
+
 def is_visit_long(visit, minutes=60):
     leaved_at = visit.leaved_at if visit.leaved_at else timezone.now()
-    time_in_building = leaved_at - visit.entered_at    
+    time_in_building = leaved_at - visit.entered_at
     is_long = time_in_building.total_seconds() / 60 > minutes
     return is_long
+
 
 def passcard_info_view(request, passcode):
     passcard = Passcard.objects.filter(passcode=passcode).all()[0]
@@ -23,14 +26,14 @@ def passcard_info_view(request, passcode):
     for visit in passcard_visits:
         this_passcard_visits.append(
             {
-            "entered_at": visit.entered_at,
-            "duration": get_duration(visit),
-            "is_strange": is_visit_long(visit)
+                'entered_at': visit.entered_at,
+                'duration': get_duration(visit),
+                'is_strange': is_visit_long(visit)
             }
         )
 
     context = {
-        "passcard": passcard,
-        "this_passcard_visits": this_passcard_visits
+        'passcard': passcard,
+        'this_passcard_visits': this_passcard_visits
     }
     return render(request, 'passcard_info.html', context)
